e94b294f4fe3bff0155119a3fae17a177cd2571a
Ashutosh Behera <ashutoshb8888@gmail.com>
2023-11-04 00:07:22 +0530

    POM Hybdrid Framework Using Page Factory

---
+++ src/main/java/com/qa/openCart/utils/Utilities.java
@@ -0,0 +1,124 @@
+package com.qa.openCart.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class Utilities {
+	
+	public static final int IMPLICIT_WAIT_TIME=10;
+	public static final int PAGE_LOAD_TIME=5;
+	
+	private static Workbook book;
+	private static Sheet sheet;
+	
+	public static String TEST_DATA_SHEET_PATH = "./src/main/java/com/qa/openCart/testdata/TutorialsNinjaTestData.xlsx";
+
+	
+	public static String generateEmailWithTimeStamp() {
+		
+		Date date = new Date();
+		String timestamp = date.toString().replace(" ","_").replace(":","_");
+		return "abtest"+timestamp+"@test.com";
+		
+	}
+	
+	public static Object[][] getTestDataFromExcel(String sheetName) {		
+		Object data[][] = null;
+		try {
+			FileInputStream ip = new FileInputStream(TEST_DATA_SHEET_PATH);
+			book = WorkbookFactory.create(ip);
+			sheet = book.getSheet(sheetName);
+
+			data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+			for (int i=0; i<sheet.getLastRowNum(); i++) {
+				for (int j = 0; j < sheet.getRow(0).getLastCellNum(); j++) {
+					data[i][j] = sheet.getRow(i + 1).getCell(j).toString();
+				}
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}  catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return data;
+	}
+	
+	
+	//++++++++++++++++++++++++++++++++++++++Old Code
+	/*public static Object[][] getTestDataFromExcel(String sheetName) {
+		File excelFile = new File(System.getProperty("user.dir")+"\\src\\main\\java\\com\\qa\\openCart\\testdata\\TutorialsNinjaTestData.xlsx");
+		XSSFWorkbook workbook = null;
+		try {
+			FileInputStream fisExcel = new FileInputStream(excelFile);
+			workbook = new XSSFWorkbook(fisExcel);
+		}catch(Throwable e) {
+			e.printStackTrace();
+		}
+		
+		XSSFSheet sheet = workbook.getSheet(sheetName);
+		
+		int rows = sheet.getLastRowNum();
+		int cols = sheet.getRow(0).getLastCellNum();
+		
+		Object[][] data = new Object[rows][cols];
+		
+		for(int i=0;i<rows;i++) {
+			
+			XSSFRow row = sheet.getRow(i+1);
+			
+			for(int j=0;j<cols;j++) {
+				
+				XSSFCell cell = row.getCell(j);
+				CellType cellType = cell.getCellType();
+				
+				switch(cellType) {
+				
+				case STRING:
+					data[i][j] = cell.getStringCellValue();
+					break;
+				case NUMERIC:
+					data[i][j] = Integer.toString((int)cell.getNumericCellValue());
+					break;
+				case BOOLEAN:
+					data[i][j] = cell.getBooleanCellValue();
+					break;	
+		
+				}
+				
+			}
+			
+			
+		}
+		
+		return data;
+		
+	}*/
+	
+	public static String captureScreenshot(WebDriver driver,String testName) {
+		
+		File srcScreenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		String destinationScreenshotPath = System.getProperty("user.dir")+"\\Screenshots\\"+testName+".png";
+		
+		try {
+			FileHandler.copy(srcScreenshot,new File(destinationScreenshotPath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return destinationScreenshotPath;
+	}
+	
+}
